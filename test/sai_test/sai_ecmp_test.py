123e52144ed02a4ce25789b35f3778fa5b7c9b6b
zhiyuan-ms <liangzy512@gmail.com>
2022-08-31 09:54:44 +0000

    add loadbalance on src ip
    Signed-off-by: zhiyuan-ms <liangzy512@gmail.com>

--- test/sai_test/sai_ecmp_test.py
+++ test/sai_test/sai_ecmp_test.py
@@ -81,7 +81,7 @@ class LoadbalanceOnSrcPortTestV4(T0TestBase):
 
         print(rcv_count)
         for i in range(0, cnt_ports):
-            self.assertTrue((rcv_count[i] >= (max_itrs / cnt_ports * 0.6)), "Not all paths are equally balanced")
+            self.assertTrue((rcv_count[i] >= (max_itrs / cnt_ports * 0.8)), "Not all paths are equally balanced")
 
     def runTest(self):
         self.test_load_balance_on_sportv4()
@@ -164,7 +164,7 @@ class LoadbalanceOnSrcPortTestV6(T0TestBase):
 
         print(rcv_count)
         for i in range(0, cnt_ports):
-            self.assertTrue((rcv_count[i] >= (max_itrs / cnt_ports * 0.6)), "Not all paths are equally balanced")
+            self.assertTrue((rcv_count[i] >= (max_itrs / cnt_ports * 0.8)), "Not all paths are equally balanced")
 
     def runTest(self):
         self.test_load_balance_on_sportv6()
@@ -252,7 +252,7 @@ class LoadbalanceOnDstPortTestV4(T0TestBase):
 
         print(rcv_count)
         for i in range(0, cnt_ports):
-            self.assertTrue((rcv_count[i] >= (max_itrs / cnt_ports * 0.6)), "Not all paths are equally balanced")
+            self.assertTrue((rcv_count[i] >= (max_itrs / cnt_ports * 0.8)), "Not all paths are equally balanced")
 
     def runTest(self):
         self.test_load_balance_on_dportv4()
@@ -335,10 +335,170 @@ class LoadbalanceOnDstPortTestV6(T0TestBase):
 
         print(rcv_count)
         for i in range(0, cnt_ports):
-            self.assertTrue((rcv_count[i] >= (max_itrs / cnt_ports * 0.6)), "Not all paths are equally balanced")
+            self.assertTrue((rcv_count[i] >= (max_itrs / cnt_ports * 0.8)), "Not all paths are equally balanced")
 
     def runTest(self):
         self.test_load_balance_on_dportv6()
 
     def tearDown(self):
         super().tearDown()
+
+
+class LoadbalanceOnSrcIPTestV4(T0TestBase):
+    """
+    Verify loadbalance on NexthopGroup ipv4 by source port.
+    """
+
+    def setUp(self):
+        """
+        Test the basic setup process
+        """
+        T0TestBase.setUp(self,
+                         is_create_route_for_nhopgrp=True,
+                         is_reuse_lag_for_nhopgrp=False,
+                        )
+        
+    def test_load_balance_on_sipv4(self):
+        """
+        1. Generate different packets by updating src ip (192.168.0.1-192.168.0.10)
+        2. send these packets on port1
+        3. Check if packets are received on ports of lag1-4 equally
+        """
+        print("Ecmp l3 load balancing test based on src ip")
+        max_itrs = 400
+        recv_dev_port_idxs = self.get_dev_port_indexes(
+            list(filter(lambda item: item != 1, self.dut.nhop_groupv4_list[0].member_port_indexs)))
+        cnt_ports = len(recv_dev_port_idxs)
+        rcv_count = [0 for _ in range(cnt_ports)]
+
+        ip_dst = self.servers[60][1].ipv4
+        for index in range(0, max_itrs):
+            ip_index = index % 10
+            ip_src = self.servers[0][ip_index+1].ipv4
+            pkt = simple_tcp_packet(eth_dst=ROUTER_MAC,
+                                    eth_src=self.servers[1][1].mac,
+                                    ip_dst=ip_dst,
+                                    ip_src=ip_src,
+                                    ip_id=105,
+                                    ip_ttl=64)
+
+            exp_pkt1 = simple_tcp_packet(eth_dst=self.t1_list[1][100].mac,
+                                         eth_src=ROUTER_MAC,
+                                         ip_dst=ip_dst,
+                                         ip_src=ip_src,
+                                         ip_id=105,
+                                         ip_ttl=63)
+
+            exp_pkt2 = simple_tcp_packet(eth_dst=self.t1_list[2][100].mac,
+                                         eth_src=ROUTER_MAC,
+                                         ip_dst=ip_dst,
+                                         ip_src=ip_src,
+                                         ip_id=105,
+                                         ip_ttl=63)
+
+            exp_pkt3 = simple_tcp_packet(eth_dst=self.t1_list[3][100].mac,
+                                         eth_src=ROUTER_MAC,
+                                         ip_dst=ip_dst,
+                                         ip_src=ip_src,
+                                         ip_id=105,
+                                         ip_ttl=63) 
+
+            exp_pkt4 = simple_tcp_packet(eth_dst=self.t1_list[4][100].mac,
+                                         eth_src=ROUTER_MAC,
+                                         ip_dst=ip_dst,
+                                         ip_src=ip_src,
+                                         ip_id=105,
+                                         ip_ttl=63) 
+
+            send_packet(self, self.dut.port_obj_list[1].dev_port_index, pkt)
+            rcv_idx= verify_any_packet_any_port(
+                self, [exp_pkt1, exp_pkt2, exp_pkt3, exp_pkt4], recv_dev_port_idxs)
+            rcv_count[rcv_idx] += 1
+
+        print(rcv_count)
+        for i in range(0, cnt_ports):
+            self.assertTrue((rcv_count[i] >= (max_itrs / cnt_ports * 0.8)), "Not all paths are equally balanced")
+
+    def runTest(self):
+        self.test_load_balance_on_sipv4()
+
+    def tearDown(self):
+        super().tearDown()
+
+
+class LoadbalanceOnSrcIPTestV6(T0TestBase):
+    """
+    Verify loadbalance on NexthopGroup ipv6 by source port.
+    """
+
+    def setUp(self):
+        """
+        Test the basic setup process
+        """
+        T0TestBase.setUp(self,
+                         is_create_route_for_nhopgrp=True,
+                         is_reuse_lag_for_nhopgrp=False,
+                        )
+        
+    def test_load_balance_on_sipv6(self):
+        """
+        1. Generate different packets by updating src ip (192.168.0.1-192.168.0.10)
+        2. send these packets on port1
+        3. Check if packets are received on ports of lag1-4 equally
+        """
+        print("Ecmp l3 load balancing test based on src ip")
+        max_itrs = 400
+        recv_dev_port_idxs = self.get_dev_port_indexes(
+            list(filter(lambda item: item != 1, self.dut.nhop_groupv6_list[0].member_port_indexs)))
+        cnt_ports = len(recv_dev_port_idxs)
+        rcv_count = [0 for _ in range(cnt_ports)]
+
+        ip_dst = self.servers[60][1].ipv6
+        for index in range(0, max_itrs):
+            ip_index = index % 10
+            ip_src = self.servers[0][ip_index+1].ipv6
+            pkt = simple_tcpv6_packet(eth_dst=ROUTER_MAC,
+                                    eth_src=self.servers[1][1].mac,
+                                    ipv6_dst=ip_dst,
+                                    ipv6_src=ip_src,
+                                    ipv6_hlim=64)
+
+            exp_pkt1 = simple_tcpv6_packet(eth_dst=self.t1_list[1][100].mac,
+                                         eth_src=ROUTER_MAC,
+                                         ipv6_dst=ip_dst,
+                                         ipv6_src=ip_src,
+                                         ipv6_hlim=63)
+
+            exp_pkt2 = simple_tcpv6_packet(eth_dst=self.t1_list[2][100].mac,
+                                         eth_src=ROUTER_MAC,
+                                         ipv6_dst=ip_dst,
+                                         ipv6_src=ip_src,
+                                         ipv6_hlim=63)
+
+            exp_pkt3 = simple_tcpv6_packet(eth_dst=self.t1_list[3][100].mac,
+                                         eth_src=ROUTER_MAC,
+                                         ipv6_dst=ip_dst,
+                                         ipv6_src=ip_src,
+                                         ipv6_hlim=63) 
+
+            exp_pkt4 = simple_tcpv6_packet(eth_dst=self.t1_list[4][100].mac,
+                                         eth_src=ROUTER_MAC,
+                                         ipv6_dst=ip_dst,
+                                         ipv6_src=ip_src,
+                                         ipv6_hlim=63) 
+
+            send_packet(self, self.dut.port_obj_list[1].dev_port_index, pkt)
+            rcv_idx= verify_any_packet_any_port(
+                self, [exp_pkt1, exp_pkt2, exp_pkt3, exp_pkt4], recv_dev_port_idxs)
+            rcv_count[rcv_idx] += 1
+
+        print(rcv_count)
+        for i in range(0, cnt_ports):
+            self.assertTrue((rcv_count[i] >= (max_itrs / cnt_ports * 0.8)), "Not all paths are equally balanced")
+
+    def runTest(self):
+        self.test_load_balance_on_sipv6()
+
+    def tearDown(self):
+        super().tearDown()
+
